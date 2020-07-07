26634eeb7a9d7b5a8c7ba5ad19a0c68706ea88a9
unknown <wablepriyanka5@gmail.com>
2020-07-07 19:06:31 +0530

    first slenium commit

---
+++ src/git_test.java
@@ -0,0 +1,11 @@
+
+public class git_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("first git test");
+		System.out.println("first selenium project");
+		
+	}
+
+}
